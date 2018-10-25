51304bb3ad4089e659a5f1b240d2fc02bddb5720
Michele Triaca <michele.triaca@telnext.com>
2018-10-25 20:46:14 +0200

    [statusbar] show currently logged in user

--- src/statusbar/index.ts
+++ src/statusbar/index.ts
@@ -1,9 +1,14 @@
 import * as vscode from 'vscode'
 import * as elegantSpinner from 'elegant-spinner'
 import { DoneCallback } from '../fast-sfdc'
+import configService from '../services/config-service'
 
 const sbItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 5)
-const MENU_PREFIX = 'FastSfdc'
+const MENU_PREFIX = () => {
+  const cfg = configService.getConfigSync()
+  return cfg.stored ? `FastSfdc - ${cfg.credentials[cfg.currentCredential].username}` : 'FastSfdc - not logged in'
+}
+
 const spinner = elegantSpinner()
 
 let loadingTimer: NodeJS.Timer
@@ -11,17 +16,17 @@ let loadingCounter = 0
 
 export default {
   initStatusBar () {
-    sbItem.text = MENU_PREFIX
+    sbItem.text = MENU_PREFIX()
     sbItem.show()
   },
 
   startLoading () {
-    if (!loadingCounter++) loadingTimer = setInterval(() => sbItem.text = `${MENU_PREFIX}: ${spinner()}`, 50)
+    if (!loadingCounter++) loadingTimer = setInterval(() => sbItem.text = `${MENU_PREFIX()}: ${spinner()}`, 50)
   },
 
   stopLoading () {
     if (loadingCounter === 1) {
-      sbItem.text = MENU_PREFIX
+      sbItem.text = MENU_PREFIX()
       clearInterval(loadingTimer)
     }
     loadingCounter = Math.max(loadingCounter - 1, 0)
@@ -42,6 +47,6 @@ export default {
   },
 
   setText (newTxt: string) {
-    sbItem.text = `${MENU_PREFIX}${newTxt && ': ' + newTxt || ''}`
+    sbItem.text = `${MENU_PREFIX()}${newTxt && ': ' + newTxt || ''}`
   }
 }
