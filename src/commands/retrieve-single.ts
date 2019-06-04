f0118804a248f731a1e60cb1a2f996c61db77ca6
sbrivio <daedrico@gmail.com>
2019-06-05 00:01:37 +0200

    [retrieveSingle] added new command to retrieve active file in editor

---
+++ src/commands/retrieve-single.ts
@@ -0,0 +1,21 @@
+import * as vscode from 'vscode'
+import * as path from 'path'
+import sfdcConnector from '../sfdc-connector'
+import statusbar from '../statusbar'
+import * as decompress from 'decompress'
+import * as b64 from 'base64-async'
+
+export default async function retrieveSingle() {
+  statusbar.startLongJob(async done => {
+    if (!vscode.window.activeTextEditor || !vscode.window.activeTextEditor.document) return done('👎🏻')
+    const fileName = vscode.window.activeTextEditor.document.fileName
+    const srcFolder = path.resolve(vscode.workspace.rootPath as string, 'src')
+    const retrieveJob = await sfdcConnector.retrieveSingleMetadata(fileName)
+    const retrieveResult = await sfdcConnector.pollRetrieveMetadataStatus(retrieveJob.id)
+    const zipBuffer = await b64.decode(retrieveResult.zipFile)
+    await decompress(zipBuffer, srcFolder, {
+      filter: file => file.path !== 'package.xml'
+    })
+    done('👍🏻')
+  })
+}
