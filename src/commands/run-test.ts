abb438c3258518209dd74d48b5f49cbf49fe925d
Stefano Brivio <sbrivio@deloitte.it>
2020-03-05 16:07:50 +0100

    [runtest] added interfaces

--- src/commands/run-test.ts
+++ src/commands/run-test.ts
@@ -2,17 +2,28 @@ import StatusBar from '../statusbar'
 import sfdcConnector from '../sfdc-connector'
 import logger from '../logger'
 
-const printResults = (result: any) => {
-  logger.show()
+export interface Result {
+  successes: TestResult[],
+  failures: TestResult[]
+}
 
+export interface TestResult {
+  name: string,
+  methodName: string,
+  message: string,
+  stackTrace: string
+}
+
+const printResults = (result: Result) => {
   logger.appendLine(`*** Test execution results ***`)
-  result.successes.forEach((v: any) => {
+  result.successes.forEach((v: TestResult) => {
     logger.appendLine(`${v.name}.${v.methodName} - OK`)
   })
 
-  result.failures.forEach((v: any) => {
+  result.failures.forEach((v: TestResult) => {
     logger.appendLine(`${v.name}.${v.methodName} - KO: ${v.message}. ${v.stackTrace}`)
   })
+  logger.show()
 }
 
 export default async function runTest (className: string, methodName: string) {
