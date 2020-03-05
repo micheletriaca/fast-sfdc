5169f69af0aa71ea4a80c5ca11e5fa7e85122ff5
Stefano Brivio <sbrivio@deloitte.it>
2020-03-05 10:53:38 +0100

    [codelens] showing codelens at @isTest

--- src/codelens-provider.ts
+++ src/codelens-provider.ts
@@ -2,13 +2,18 @@ import * as vscode from 'vscode'
 export default class CodeLensProvider implements vscode.CodeLensProvider {
   async provideCodeLenses (document: vscode.TextDocument): Promise<vscode.CodeLens[]> {
     let codeLens: vscode.CodeLens[] = []
-    codeLens.push(new vscode.CodeLens(
-      new vscode.Range(0, 0, 0, 0),
-      {
-        command: 'FastSfdc.runTest',
-        title: 'FastSfdc - Run test'
+    const docLine = document.lineCount
+    for (let i = 0; i < docLine - 1; i++) {
+      let line = document.lineAt(i)
+      if (line.text.indexOf('@isTest') !== -1) {
+        codeLens.push(new vscode.CodeLens(line.range,
+          {
+            command: 'FastSfdc.runTest',
+            title: i === 0 ? 'FastSfdc - Run all tests' : 'FastSfdc - Run test'
+          }
+        ))
       }
-    ))
+    }
     return codeLens
   }
 }
