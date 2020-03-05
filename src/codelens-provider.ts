48e47b0ee70aa468222ac509f48cd38dd1a9b0c4
Stefano Brivio <sbrivio@deloitte.it>
2020-03-05 12:15:04 +0100

    [codelens] passing classname and methodname params

--- src/codelens-provider.ts
+++ src/codelens-provider.ts
@@ -1,15 +1,42 @@
 import * as vscode from 'vscode'
+import parsers from './utils/parsers'
+
 export default class CodeLensProvider implements vscode.CodeLensProvider {
   async provideCodeLenses (document: vscode.TextDocument): Promise<vscode.CodeLens[]> {
+    const filename = parsers.getFilename(document.fileName)
+    const regex = new RegExp('.*\(\)\s*{.*')
+
+    // non va bene perché le righe sono sempre singole
+    // const fullRegex = new RegExp('.*\@isTest.*\(\)\s*{.*', 's')
+
     let codeLens: vscode.CodeLens[] = []
     const docLine = document.lineCount
+    let counter = 0 // primo @isTest rappresenta tutta la classe
     for (let i = 0; i < docLine - 1; i++) {
       let line = document.lineAt(i)
+
+      // vanno skippate le righe commentate
       if (line.text.indexOf('@isTest') !== -1) {
+        counter++
+
+        let methodName: string = ''
+        if (counter > 1) {
+          if (regex.test(line.text)) {
+            methodName = parsers.getMethodName(line.text)
+          } else {
+            // verificare che next esista
+            let nextLine = document.lineAt(i + 1)
+            if (regex.test(nextLine.text)) {
+              methodName = parsers.getMethodName(nextLine.text)
+            }
+          }
+        }
+
         codeLens.push(new vscode.CodeLens(line.range,
           {
             command: 'FastSfdc.runTest',
-            title: i === 0 ? 'FastSfdc - Run all tests' : 'FastSfdc - Run test'
+            title: i === 0 ? 'FastSfdc - Run all tests' : 'FastSfdc - Run test',
+            arguments: [filename, methodName]
           }
         ))
       }
--- src/commands/run-test.ts
+++ src/commands/run-test.ts
@@ -1,25 +1,24 @@
 import * as vscode from 'vscode'
 import StatusBar from '../statusbar'
-import parsers from '../utils/parsers'
 import sfdcConnector from '../sfdc-connector'
 
-export default async function runTest () {
-  if (!vscode.window.activeTextEditor || !vscode.window.activeTextEditor.document) return
-  const filename = parsers.getFilename(vscode.window.activeTextEditor.document.fileName)
-
+export default async function runTest (className: string, methodName: string) {
   StatusBar.startLongJob(async done => {
     let res: any = null
+    let request: any = { className }
+    if (methodName) request.testMethods = [methodName]
     try {
-      res = await sfdcConnector.runTestSync([{ className: filename }])
+      res = await sfdcConnector.runTestSync([request])
     } catch (e) {
       return done(e)
     }
-    console.log('run finish')
-    console.log(res)
     done('👍🏻')
 
     const docRes = await vscode.workspace.openTextDocument({
-      content: JSON.stringify(res, null, 2),
+      content: JSON.stringify({
+        successes: res.successes,
+        failures: res.failures
+      }, null, 2),
       language: 'json'
     })
     await vscode.window.showTextDocument(docRes)
