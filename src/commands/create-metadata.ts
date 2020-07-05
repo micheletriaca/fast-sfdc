5bcfebdc41db48142d171da13ae2a7c111bd6f5d
Konstantinos Mantos <makostas1996@gmail.com>
2020-07-06 02:26:45 +0300

    [includeHtml] Include HTML file on LWC creation

--- src/commands/create-metadata.ts
+++ src/commands/create-metadata.ts
@@ -25,13 +25,13 @@ async function chooseType (): Promise<DocType | undefined> {
 
 function getDocument (metaType: string, metaName: string, objName?: string) {
   switch (metaType) {
-    case 'ApexClassMember': return `public class ${metaName} {\n\n}`
-    case 'ApexPageMember': return '<apex:page>\nHello world!\n</apex:page>'
-    case 'ApexComponentMember': return '<apex:component>\nHello world!\n</apex:component>'
-    case 'ApexTriggerMember': return `trigger ${metaName} on ${objName} (before insert) {\n\n}`
-    case 'AuraDefinitionBundle': return '<aura:component ' +
-      'implements="flexipage:availableForRecordHome,force:hasRecordId" access="global">\n\n</aura:component>'
-    case 'LightningComponentBundle': return 'import { LightningElement } from \'lwc\'\nexport default class CmpCtrl extends LightningElement {\n\n}'
+    case 'ApexClassMember': return `public class ${metaName} {\n  \n}`
+    case 'ApexPageMember': return '<apex:page>\n  \n</apex:page>'
+    case 'ApexComponentMember': return '<apex:component>\n  \n</apex:component>'
+    case 'ApexTriggerMember': return `trigger ${metaName} on ${objName} (before insert) {\n  \n}`
+    case 'AuraDefinitionBundle': return '<aura:component implements="flexipage:availableForRecordHome,force:hasRecordId" access="global">\n  \n</aura:component>'
+    case 'LightningComponentBundle': return `import { LightningElement } from 'lwc'\n\nexport default class ${metaName[0].toUpperCase() + metaName.slice(1)} extends LightningElement {\n  \n}`
+    case 'LightningComponentBundleHtml': return `<template>${'\n  \n'}</template>`
     default: return ''
   }
 }
@@ -71,7 +71,7 @@ async function createRemoteAuraBundle (docBody: string, docMeta: AuraMetadata, d
   return auraCmpId
 }
 
-async function createRemoteLwcBundle (docBody: string, docMeta: LwcMetadata, docName: string) {
+async function createRemoteLwcBundle (docBody: string, docBodyHtml: string, docMeta: LwcMetadata, docName: string) {
   const lwcBundleId = await sfdcConnector.upsertObj('LightningComponentBundle', {
     Metadata: docMeta,
     FullName: docName
@@ -82,6 +82,14 @@ async function createRemoteLwcBundle (docBody: string, docMeta: LwcMetadata, doc
     LightningComponentBundleId: lwcBundleId,
     Format: 'js'
   })
+  if (docBodyHtml) {
+    await sfdcConnector.upsertLwcObj({
+      FilePath: `lwc/${docName}/${docName}.html`,
+      Source: docBodyHtml,
+      LightningComponentBundleId: lwcBundleId,
+      Format: 'js'
+    })
+  }
   await sfdcConnector.upsertLwcObj({
     FilePath: `lwc/${docName}/${docName}.js-meta.xml`,
     Source: buildXml({
@@ -109,12 +117,15 @@ async function createRemoteMeta (docBody: string, docMeta: AnyMetadata, docName:
   }
 }
 
-async function storeOnFileSystem (docBody: string, docMeta: AnyMetadata, docName: string, docType: DocType) {
+async function storeOnFileSystem (docBody: string, docBodyHtml: string, docMeta: AnyMetadata, docName: string, docType: DocType) {
   const isAuraBundle = docType.toolingType === 'AuraDefinitionBundle' || docType.toolingType === 'LightningComponentBundle'
   let p = path.join(utils.getWorkspaceFolder(), 'src', docType.folder, docName + docType.extension)
   if (isAuraBundle) {
     const bundleDirPath = path.join(utils.getWorkspaceFolder(), 'src', docType.folder, docName)
     p = path.join(bundleDirPath, docName + docType.extension)
+  }
+  if (docBodyHtml) {
+    await utils.writeFile(`${p.slice(0, -3)}.html`, docBodyHtml)
   }
   await utils.writeFile(p, docBody)
   await utils.writeFile(`${p}-meta.xml`, buildXml({
@@ -134,6 +145,14 @@ async function storeOnFileSystem (docBody: string, docMeta: AnyMetadata, docName
   await vscode.window.showTextDocument(await vscode.workspace.openTextDocument(vscode.Uri.file(p)))
 }
 
+async function showWithHtmlMenu (): Promise<boolean> {
+  const res = await vscode.window.showQuickPick(
+    [{ label: 'Yes', value: true }, { label: 'No', value: false }],
+    { ignoreFocusOut: true, placeHolder: 'Do you want to create .html file along with the LWC?' }
+  )
+  return !!res && res.value
+}
+
 export default async function createMeta () {
   const docType = await chooseType()
   if (!docType) return
@@ -146,16 +165,20 @@ export default async function createMeta () {
   if (isTrigger && !sObjectName) return
 
   const docBody = getDocument(docType.toolingType, docName, sObjectName)
+  const withHtml = docType.toolingType === 'LightningComponentBundle'
+    ? await showWithHtmlMenu()
+    : false
+  const docBodyHtml = withHtml ? getDocument('LightningComponentBundleHtml', docName, sObjectName) : ''
   const docMeta = getMetadata(docType.toolingType, docName, await configService.getPackageXmlVersion())
 
   StatusBar.startLongJob(async done => {
     try {
       switch (docType.toolingType) {
         case 'AuraDefinitionBundle': await createRemoteAuraBundle(docBody, docMeta as AuraMetadata, docName); break
-        case 'LightningComponentBundle': await createRemoteLwcBundle(docBody, docMeta as LwcMetadata, docName); break
+        case 'LightningComponentBundle': await createRemoteLwcBundle(docBody, docBodyHtml, docMeta as LwcMetadata, docName); break
         default: await createRemoteMeta(docBody, docMeta, docName, docType)
       }
-      await storeOnFileSystem(docBody, docMeta, docName, docType)
+      await storeOnFileSystem(docBody, docBodyHtml, docMeta, docName, docType)
       done('👍🏻')
     } catch (e) {
       vscode.window.showErrorMessage(e.message)
