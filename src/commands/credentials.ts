3eaac0167da009314b193be89f59278dab447a80
Michele Triaca <michele.triaca@telnext.com>
2020-07-24 00:33:55 +0200

    [addcredential] add to gitignore only if config file does not exist

--- src/commands/credentials.ts
+++ src/commands/credentials.ts
@@ -56,13 +56,16 @@ export default async function enterCredentials (addMode = false) {
     config.currentCredential = config.credentials.length - 1
   }
 
-  await configService.storeConfig(config)
-  vscode.commands.executeCommand('setContext', 'fast-sfdc-configured', true)
-
-  if (config.credentials.length === 1) {
+  if (!config.stored) {
     try {
-      const editGitIgnore = await vscode.window.showQuickPick([{ label: 'Yes', value: true }, { label: 'No', value: false }], { ignoreFocusOut: true, placeHolder: 'Would you like to add fastsfdc config file to .gitignore?' })
-      if (editGitIgnore && editGitIgnore.value) {
+      const editGitIgnore = await vscode.window.showQuickPick([
+        { label: 'Yes', value: true },
+        { label: 'No', value: false }
+      ], {
+        ignoreFocusOut: true,
+        placeHolder: 'Would you like to add fastsfdc config file to .gitignore?'
+      })
+      if (editGitIgnore?.value) {
         const gitIgnorePath = path.join(utils.getWorkspaceFolder(), '.gitignore')
         fs.appendFileSync(gitIgnorePath, `\n**/${configService.getConfigFileName()}\n`)
       }
@@ -70,6 +73,9 @@ export default async function enterCredentials (addMode = false) {
       vscode.window.showErrorMessage('There was a problem updating the .gitignore file')
     }
   }
+
+  await configService.storeConfig(config)
+  vscode.commands.executeCommand('setContext', 'fast-sfdc-configured', true)
 
   StatusBar.startLongJob(async done => {
     try {
