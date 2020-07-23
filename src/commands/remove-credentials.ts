0513c6dc021e49fecf5d3f4d9e9fc490f74f13b2
Michele Triaca <michele.triaca@telnext.com>
2020-07-24 00:19:45 +0200

    [removecredential] fix currentcredential index

--- src/commands/remove-credentials.ts
+++ src/commands/remove-credentials.ts
@@ -5,8 +5,8 @@ import { ConfigCredential } from '../fast-sfdc'
 async function showCredsMenu (credentials: ConfigCredential[], currentCredential: number): Promise<string | undefined> {
   return (await vscode.window.showQuickPick(credentials
     .filter((x, idx) => idx !== currentCredential)
-    .map((x, idx) => ({ label: '$(person) ' + x.username!, idx }))
-  ))?.label
+    .map((x, idx) => ({ val: x.username, label: '$(person) ' + x.username!, idx }))
+  , { placeHolder: 'Select credential to remove' }))?.val
 }
 
 export default async function removeCredentials () {
@@ -14,6 +14,13 @@ export default async function removeCredentials () {
   const selUsername = await showCredsMenu(config.credentials, config.currentCredential)
   if (!selUsername) return
 
-  await configService.storeConfig({ ...config, credentials: config.credentials.filter(x => x.username !== selUsername) })
+  const credToRemoveIdx = config.credentials.findIndex(x => x.username === selUsername)
+  const currentIdx = config.currentCredential
+
+  await configService.storeConfig({
+    ...config,
+    credentials: config.credentials.filter(x => x.username !== selUsername),
+    currentCredential: credToRemoveIdx < currentIdx ? currentIdx - 1 : currentIdx
+  })
   vscode.window.showInformationMessage('Credential removed!')
 }
